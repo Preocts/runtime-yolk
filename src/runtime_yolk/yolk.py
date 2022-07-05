2829427eeefe4ab81a1e6616b6628bae68a4dc96
Preocts <preocts@gmail.com>
2022-07-05 16:02:31 -0400

    Placeholder for main class

---
+++ src/runtime_yolk/yolk.py
@@ -0,0 +1,13 @@
+"""
+Main class for the runtime-yolk project.
+
+Reponsible for all loaders and runners.
+"""
+from __future__ import annotations
+
+
+class Yolk:
+    """Create a single class for run-time initiation tasks."""
+
+    def __init__(self) -> None:
+        pass
