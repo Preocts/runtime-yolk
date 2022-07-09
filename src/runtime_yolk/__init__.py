22f0f43e06ca6c59e6d956a8b917b210253fce29
Preocts <preocts@gmail.com>
2022-07-08 22:58:33 -0400

    Forbidden init file loading

---
+++ src/runtime_yolk/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from runtime_yolk.config_loader import ConfigLoader
+from runtime_yolk.env_loader import EnvLoader
+from runtime_yolk.yolk import Yolk
+
+__all__ = [
+    "ConfigLoader",
+    "EnvLoader",
+    "Yolk",
+]
