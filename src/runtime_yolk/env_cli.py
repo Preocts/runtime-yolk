ee7b641f8db698ee45ffe5274f78aecf7647b6c3
Preocts <preocts@gmail.com>
2022-07-17 21:55:47 -0400

    Fill out options, add entry point

--- src/runtime_yolk/env_cli.py
+++ src/runtime_yolk/env_cli.py
@@ -7,15 +7,47 @@ from argparse import Namespace
 def _parse_args(arg_list: list[str] | None = None) -> Namespace:
     """Parse sys.argv."""
     parser = ArgumentParser("Add, update, or remove env values from .env file.")
-    parser.add_argument("key", type=str, help="Name of environ variable to save.")
-    parser.add_argument("value", type=str, default="", nargs="?", help="Value to save.")
-    parser.add_argument("-U", "--update", action="store_const", const=True)
-    parser.add_argument("-D", "--delete", action="store_const", const=True)
+    parser.add_argument(
+        "key",
+        type=str,
+        help="Name of environ variable to save.",
+    )
+    parser.add_argument(
+        "value",
+        type=str,
+        default="",
+        nargs="?",
+        help="Value to save.",
+    )
+    parser.add_argument(
+        "-U",
+        "--update",
+        action="store_true",
+        help="Update existing value.",
+    )
+    parser.add_argument(
+        "-D",
+        "--delete",
+        action="store_true",
+        help="Delete existing key.",
+    )
+    parser.add_argument(
+        "-F",
+        "--file",
+        action="store",
+        default=".env",
+        help="Specify filename and path, default is '.env'",
+    )
     return parser.parse_args(arg_list)
 
 
 def main() -> int:
     """Entry point for cli."""
     # pragma: no cover
-
+    args = _parse_args()
+    print(args)
     return 1
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
--- tests/env_cli_test.py
+++ tests/env_cli_test.py
@@ -4,13 +4,14 @@ from runtime_yolk import env_cli
 
 
 def test_parse_args() -> None:
-    cli_args = ["test_key", "test_value"]
+    cli_args = ["test_key", "test_value", "-F", "tests/fixtures/env_cli_test/.env"]
 
     args = env_cli._parse_args(cli_args)
 
     assert args.key == "test_key"
     assert args.value == "test_value"
     assert args.update is not True
+    assert args.file == "tests/fixtures/env_cli_test/.env"
 
 
 def test_parse_args_update() -> None:
@@ -22,6 +23,7 @@ def test_parse_args_update() -> None:
 
     assert args.update is True
     assert args_flags.update is True
+    assert args.file == ".env"
 
 
 def test_parse_args_delete() -> None:
